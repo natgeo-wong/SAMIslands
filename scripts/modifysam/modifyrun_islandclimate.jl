4557b7a6d0f97ce14d3ac69f73b18c1617096f3e
Nathanael Wong <natgeo.wong@outlook.com>
2023-08-06 19:43:15 +0800

    Adding idealised RCE island experiments

---
+++ scripts/modifysam/modifyrun_islandclimate.jl
@@ -0,0 +1,52 @@
+using DrWatson
+@quickactivate "TropICS"
+using Printf
+
+rvec   = [10,15,20,30,50,70,100]
+mldvec = [0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10]
+
+mrun = projectdir("run","modifysam","runtemplates","modelrun.sh")
+brun = projectdir("run","modifysam","runtemplates","Build.csh")
+
+open(mrun,"r") do frun
+    s = read(frun,String)
+    for r in rvec, mld in mldvec
+
+        rstr   = @sprintf("%03d",r)
+        mldstr = @sprintf("%05.2f",mld)
+        runname = "r_$(rstr)km-mld_$(mldstr)m"
+        nrun = projectdir("run","IslandClimate","$(runname).sh")
+
+        open(nrun,"w") do wrun
+            sn = replace(s ,"[email]"   => )
+            sn = replace(sn,"[prjname]" => projectdir())
+            sn = replace(sn,"[expname]" => "IslandClimate")
+            sn = replace(sn,"[runname]" => runname)
+            sn = replace(sn,"[sndname]" => "control")
+            sn = replace(sn,"[lsfname]" => "control")
+            write(wrun,sn)
+        end
+
+    end
+end
+
+open(brun,"r") do frun
+    s = read(frun,String)
+    nrun = projectdir("run","IslandClimate","Build.csh")
+    open(nrun,"w") do wrun
+        sn = replace(s ,"[datadir]" => datadir())
+        sn = replace(sn,"[expname]" => "IslandClimate")
+        write(wrun,sn)
+    end
+
+    for r in rvec, mld in mldvec
+
+        rstr   = @sprintf("%03d",r)
+        mldstr = @sprintf("%05.2f",mld)
+        runname = "r_$(rstr)km-mld_$(mldstr)m"
+        mkpath(datadir("IslandClimate","OUT_2D",runname))
+        mkpath(datadir("IslandClimate","OUT_3D",runname))
+        mkpath(datadir("IslandClimate","OUT_STAT",runname))
+
+    end
+end
---
+++ scripts/modifysam/runtemplates/modelrun.sh
@@ -0,0 +1,53 @@
+#!/bin/sh 
+
+##SBATCH -p test         # short jobs, time limit 8 hours
+##SBATCH -p huce_cascade # default, moderate, no time limit
+##SBATCH -p huce_ice     # expensive, faster, no time limit
+##SBATCH -p shared       # longer jobs, 7 days, use only when needed
+
+#SBATCH -N 2 # number of nodes
+#SBATCH -n 64 # number of cores
+#SBATCH --mem-per-cpu=500 # memory pool for each core
+#SBATCH -t 0-12:00 # time (D-HH:MM)
+
+##SBATCH --account=linz_lab
+#SBATCH -J "SAM_WTG"
+#SBATCH --mail-user=[email]
+#SBATCH --mail-type=ALL
+#SBATCH -o ./LOGS/samrun.%j.out # STDOUT
+#SBATCH -e ./LOGS/samrun.%j.err # STDERR
+
+module purge
+module load intel/23.0.0-fasrc01 intelmpi/2021.8.0-fasrc01 netcdf-fortran/4.6.0-fasrc03
+
+case=RCE
+expname=[expname]
+runname=[runname]
+sndname=[sndname]
+lsfname=[lsfname]
+
+exproot=[prjname]/exp
+prmfile=$exproot/prm/$expname/${runname}.prm
+sndfile=$exproot/snd/$sndname
+lsffile=$exproot/lsf/$lsfname
+
+prmloc=./$case/prm
+sndloc=./$case/snd
+lsfloc=./$case/lsf
+
+cp $prmfile $prmloc
+cp $sndfile $sndloc
+cp $lsffile $lsfloc
+
+scriptdir=$SLURM_SUBMIT_DIR
+SAMname=`ls $scriptdir/SAM_*`
+echo $case > CaseName
+
+cd $scriptdir
+export OMPI_MCA_btl="self,openib"
+mpirun -np $SLURM_NTASKS $SAMname > ./LOGS/samrun.${SLURM_JOBID}.log
+
+exitstatus=$?
+echo SAM stopped with exit status $exitstatus
+
+exit 0
